23e09feca1898086807375b9df719442bbbb613e
Zoe Wang <zooeee2011@gmail.com>
2015-09-19 18:11:45 -0400

    auto_complete update
    Updated auto_complete.py and added a bit of documentation

--- auto_complete.py
+++ auto_complete.py
@@ -1,15 +1,24 @@
 import json
 import requests
 
-city = raw_input("city")
-url = "https://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey=mbZfPQ5trh2G97whOJeTxGwB5MiHkkoV&term=" + city
-response = requests.get(url)
-parsed = json.loads(response.text)
-n = 0
-try:
-	while parsed[n]:
-		print parsed[n]["value"]
-		n += 1
-except IndexError:
-	pass
-	
+'''
+The function returns the IATA codes for the corresponding 
+'''
+def main():
+	city = raw_input("city")
+	codes(city)
+
+def codes(city):
+	url = "https://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey=mbZfPQ5trh2G97whOJeTxGwB5MiHkkoV&term=" + city
+	response = requests.get(url)
+	parsed = json.loads(response.text)	
+	n = 0
+	try:
+		while parsed[n]:
+			print parsed[n]["value"]
+			n += 1
+	except IndexError:
+		pass
+
+if __name__ == "__main__":
+	main()
