b5abec97ceff4a27bf4ef27408206dfc823f3286
Zoe Wang <zooeee2011@gmail.com>
2015-09-23 02:04:29 -0400

    Added arrival and departure
    Fixed a bug, added arrival and departure, and will implement the track
    method next step

--- backend/flight_status.py
+++ backend/flight_status.py
@@ -10,15 +10,26 @@ def main():
 	day = times[2]
 	carrier = flight[:2]
 	number = flight[2:]
-	flight_status(carrier, number, year, month, day)
+	
+	depart = flight_departure(carrier, number, year, month, day)
+	arrive = flight_arrival(carrier, number, year, month, day)
+	print(depart, arrive)
 
-def flight_status(carrier, flight, year, month, day):
+def flight_departure(carrier, flight, year, month, day):
 	appID = "0526ef83"
 	appKEY = "bc5bcb10062dc8fac64cb3705adc318d"
 	URL = "https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/" + carrier + "/" + flight + "/dep/" + year + "/" + month + "/" + day +"?appId=0526ef83&appKey=bc5bcb10062dc8fac64cb3705adc318d&utc=false"
-	response = requests.get("https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/AA/100/dep/2015/09/22?appId=0526ef83&appKey=bc5bcb10062dc8fac64cb3705adc318d&utc=false")
+	response = requests.get(URL)
 	page = json.loads(response.text)
-	print page["flightStatuses"][0]["departureDate"]["dateLocal"]
+	return page["flightStatuses"][0]["departureDate"]["dateLocal"]
+
+def flight_arrival(carrier, flight, year, month, day):
+	URL = "https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/" + carrier + "/" + flight + "/dep/" + year + "/" + month + "/" + day +"?appId=0526ef83&appKey=bc5bcb10062dc8fac64cb3705adc318d&utc=false"
+	response = requests.get(URL)
+	page = json.loads(response.text)
+	print(page["flightStatuses"][0]["departureAirportFsCode"])
+	print(page["flightStatuses"][0]["arrivalAirportFsCode"])
+	return page["flightStatuses"][0]["arrivalDate"]["dateLocal"]
 
 if __name__ == '__main__':
 	main()
